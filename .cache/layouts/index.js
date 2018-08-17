36895c91ff49e3c8a62a24df9faa13493cb4320b
vibecastor <vibecastor@gmail.com>
2018-08-17 13:03:13 -0700

    Updates

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/michaelcastor/projects/dreaming-of-thailand-blog/src/layouts/index.js"
-  import data from "/Users/michaelcastor/projects/dreaming-of-thailand-blog/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
